4fd6da859bb4bb7eb52d4c592b156d2bd52b51b4
SAED ALLOUZI <saedallouzi@Samers-MacBook-Pro.local>
2021-05-23 21:41:58 +0300

    done

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -9,7 +9,13 @@
  * getOdds([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [11, 35, 601, 777, 999]
  */
 function getOdds(numbers) {
+  
+
+const filternumbers = numbers.filter(function(number){
+return (number % 2 !==0)}
+)
   // Your code here
+return (filternumbers)
 }
 
 /**
@@ -23,7 +29,12 @@ function getOdds(numbers) {
  * getEvens([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [52, 14, 56, 888]
  */
 function getEvens(numbers) {
+  
+const filternumbers = numbers.filter(function(number){
+return (number % 2 ===0)}
+)
   // Your code here
+return(filternumbers)
 }
 
 /**
@@ -35,9 +46,17 @@ function getEvens(numbers) {
  * countOccurences(1, [1, 2, 3, 1, 4, 5, 6, 1, 7, 8, 9, 10, 11, 1, 12, 13]) -> 4
  * countOccurences(52, [11, 35, 52, 14, 56, 601, 52, 777, 888, 999, 52]) -> 3
  */
-function countOccurences(x, numbers) {
+ function countOccurences(x, numbers) {
+  let y = 0;
+  numbers.forEach((num) => {
+    if (num === x) {
+      y++;
+    }
+  });
   // Your code here
+  return y;
 }
+
 
 /**
  * makeThemDoctors(students):
@@ -48,8 +67,10 @@ function countOccurences(x, numbers) {
  * makeThemDoctors(["Ali", "Aseel", "Richard"]) -> ["Dr. Ali", "Dr. Aseel", "Dr. Richard"]
  */
 
-function makeThemDoctors(students) {
-  // Your code here
-}
+
+  function makeThemDoctors(students) {
+    const doctors = students.map((student) => `Dr. ${student}`);
+    return doctors;
+  }
 
 module.exports = { getOdds, getEvens, countOccurences, makeThemDoctors };
